eb7c772527cfd743759097ad7b4625d6ef4b6fa0
“Cal-107” <kaly___7@hotmail.it>
2022-10-12 12:35:50 +0200

    add fix sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -18,9 +18,13 @@
         </li>
         <li>
             {{-- <i class="fa fa-id-card-o" aria-hidden="true"></i> --}}
-
             <a class="app-menu__item active" href="{{ route('admin.user.index') }}"><i class="app-menu__icon fa fa-users  "></i>
                 <span class="app-menu__label">Users</span>
+            </a>
+        </li>
+        <li>
+            <a class="app-menu__item active" href="{{ route('admin.order.index') }}"><i class="app-menu__icon fa fa-shopping-cart"></i>
+                <span class="app-menu__label">Orders</span>
             </a>
         </li>
         <li>
@@ -35,14 +39,16 @@
             </a>
         </li>
         <li>
-
             <a class="app-menu__item active" href="{{ route('admin.sellers.index') }}"><i
                     class="app-menu__icon fa fa-money"></i>
                 <span class="app-menu__label">Sellers</span>
             </a>
         </li>
-
-
+        <li>
+            <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-cogs"></i>
+                <span class="app-menu__label">Settings</span>
+            </a>
+        </li>
 
         {{--
         <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
@@ -55,8 +61,6 @@
     </a> --}}
     {{-- <i class="fa fa-pencil" aria-hidden="true"></i> --}}
 
-
-    </li>
     {{-- <li class="treeview is-expanded"><a class="app-menu__item" href="#" data-toggle="treeview"><i
                 class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i
                 class="treeview-indicator fa fa-angle-right"></i></a>
@@ -69,10 +73,5 @@
             <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
         </ul>
     </li> --}}
-    <li>
-        <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-cogs"></i>
-            <span class="app-menu__label">Settings</span>
-        </a>
-    </li>
     </ul>
 </aside>
